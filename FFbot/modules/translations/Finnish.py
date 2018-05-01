76539fbc21c380c32fb5554d4e9835a87766c7f0
ATechnoHazard <amolele@firehound.me>
2018-05-02 01:45:02 +0530

    translations: Finnish: Import antiflood module translations
    Signed-off-by: ATechnoHazard <amolele@firehound.me>

--- FFbot/modules/translations/Finnish.py
+++ FFbot/modules/translations/Finnish.py
@@ -15,4 +15,15 @@ FinnishStrings = {"Switched to {} successfully!" : "Vaihdettu suomen {} onnistun
                   "{} is now AFK!" : "{} on nyt AFK!" ,
                   "{} is no longer AFK!" : "{} ei ole enää AFK!" ,
                   "{} is AFK!" : "{} on AFK!" ,
-                  "{} is AFK! says its because of:\n{}" : "sanoo sen tämän takia:\n{}"}
+                  "{} is AFK! says its because of:\n{}" : "sanoo sen tämän takia:\n{}" ,
+                  "I like to leave the flooding to natural disasters. But you, you were just a" : "Jätän tulvat mieluummin luonnonkatastrofeille. Mutta sinä, sinä olit vain" ,
+                  "disappointment. Get out." : "pettymys. Mene ulos." ,
+                  "I can't kick people here, give me permissions first! Until then, I'll disable antiflood." : "En voi potkia ihmisiä täältä, anna minulle ensin käyttöoikeudet! Siihen asti, pidän antitulvan pois päältä." ,
+                  "Antiflood has been disabled." : "Antitulva on poistettu käytöstä." ,
+                  "Antiflood has to be either 0 (disabled), or a number bigger than 3!" : "Antitulvan täytyy olla joko 0 (pois käytöstä), tai numero suurempi kuin 3!" ,
+                  "Antiflood has been updated and set to {}" : "Antitulva on päivitetty ja asetettu kohtaan {}" ,
+                  "Unrecognised argument - please use a number, 'off', or 'no'." : "Tunnistamaton argumentti - käytäthän numeroa, 'off', tai 'ei'." ,
+                  "I'm not currently enforcing flood control!" : "En valvo tulvaohjausta juuri nyt!" ,
+                  "I'm currently banning users if they send more than {} consecutive messages." : "Tällä hetkellä annan porttikieltoja käyttäjille jos he lähettävät enemmän kuin {} peräkkäistä viestiä." ,
+                  "*Not* currently enforcing flood control." : "*En* valvo tulvaohjausta juuri nyt." ,
+                  "Antiflood is set to `{}` messages." : "Antitulva asetettu `{}` viestiin."}
