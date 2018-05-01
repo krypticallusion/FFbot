adc63fd4cc91a10c99f440eefe0db63b22697ce0
ATechnoHazard <amolele@firehound.me>
2018-05-02 01:04:33 +0530

    translations: Finnish: Import admin module translations
    Signed-off-by: ATechnoHazard <amolele@firehound.me>

--- FFbot/modules/translations/Finnish.py
+++ FFbot/modules/translations/Finnish.py
@@ -1,2 +1,13 @@
-FinnishStrings = {}
+FinnishStrings = {"How am I meant to promote someone that's already an admin?" : "Kuinka luulet minun ylentävän jonkun, joka on jo ylläpitäjä?" ,
+                  "I can't promote myself! Get an admin to do it for me." : "En voi ylentää itseäni! Hommaa admin tekemään se minulle." ,
+                  "This person CREATED the chat, how would I demote them?" : "Tämä henkilö LOI ryhmäkeskustelun, miten voisin alentaa hänet?" ,
+                  "Can't demote what wasn't promoted!" : "Ei voi alentaa sitä, mitä ei koskaan ylennetty!" ,
+                  "I can't demote myself! Get an admin to do it for me." : "En voi alentaa itseäni! Hommaa admin tekemään se minulle." ,
+                  "Successfully demoted!" : "Alennettu onnistuneesti!" ,
+                  "Could not demote. I might not be admin, or the admin status was appointed by another" : "Ei voitu alentaa. En välttämättä ole ylläpitäjä, tai joku muu myönsi ylläpitäjästatuksen" ,
+                  "user, so I can't act upon them!" : "käyttäjä, joten en voi toimia heitä kohtaan!" ,
+                  "I don't have access to the invite link, try changing my permissions!" : "Minulla ei ole valtuuksia kutsulinkkiin, kokeile muuttaa käyttöoikeuksiani!" ,
+                  "I can only give you invite links for supergroups and channels, sorry!" : "Kykenen antamaan kutsulinkkejä ainoastaan superryhmille ja kanaville, pahoittelen!" ,
+                  "Admins in *{}*:" : "Ylläpitäjät ryhmässä *{}*:" ,
+                  "You are *admin*: `{}`" : "Olet *ylläpitäjä*: `{}`"}
 
